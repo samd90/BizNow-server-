35de608e746ecaa7095b98ff8019719e461f1bf4
sameera <mudaliges90@gmail.com>
2014-10-27 12:24:20 +0530

    working server GET POST PUT DELETE

---
+++ express.test.js
@@ -0,0 +1,79 @@
+var superagent = require('superagent')
+var expect = require('expect.js')
+
+describe('express rest api server', function(){
+  var id
+
+  it('posts an object', function(done){
+    superagent.post('http://localhost:3000/collections/appoinments')
+      .send({service: 'server2'
+        , amount: '120566'})
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(res.body.length).to.eql(1)
+        expect(res.body[0]._id.length).to.eql(24)
+        id = res.body[0]._id
+        done()
+      })
+  })
+
+  it('retrieves an object', function(done){
+    superagent.get('http://localhost:3000/collections/appoinments/'+id)
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body._id.length).to.eql(24)
+        expect(res.body._id).to.eql(id)
+        done()
+      })
+  })
+
+  it('retrieves a collection', function(done){
+    superagent.get('http://localhost:3000/collections/appoinments')
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(res.body.length).to.be.above(0)
+        expect(res.body.map(function (item){return item._id})).to.contain(id)
+        done()
+      })
+  })
+
+  it('updates an object', function(done){
+    superagent.put('http://localhost:3000/collections/appoinments/'+id)
+      .send({service: 'server1'
+        , amount: '12000'})
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body.msg).to.eql('success')
+        done()
+      })
+  })
+
+  it('checks an updated object', function(done){
+    superagent.get('http://localhost:3000/collections/appoinments/'+id)
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body._id.length).to.eql(24)
+        expect(res.body._id).to.eql(id)
+        expect(res.body.service).to.eql('server1')
+        done()
+      })
+  })
+    it('removes an object', function(done){
+        superagent.del('http://localhost:3000/collections/appoinments/'+id)
+            .end(function(e, res){
+                // console.log(res.body)
+                expect(e).to.eql(null)
+                expect(typeof res.body).to.eql('object')
+                expect(res.body.msg).to.eql('success')
+                done()
+            })
+    })
+})
